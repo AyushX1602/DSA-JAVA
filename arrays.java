4351f3bf97c6b54878eab9bd2b47335814c4c802
AyushX1602 <ayushpathak16022005.com>
2025-10-07 13:52:13 +0530

    merge 2 sorted arrays

--- arrays.java
+++ arrays.java
@@ -124,29 +124,100 @@ public class arrays {
             // System.out.println("Second maximum element: " + secondMax);
 
 
-            //Reverse an array
-            int[] reversedArr = {1, 2, 3, 4, 5};
-            int n = reversedArr.length;
-            int i = 0, j = n - 1;
-            while (i < j) {
-                int temp = reversedArr[i];
-                reversedArr[i] = reversedArr[j];
-                reversedArr[j] = temp;
-                i++;
-                j--;
-            }
-            for (int element : reversedArr) {
-                System.out.print(element + " ");
-            }
+            // //Reverse an array
+            // int[] reversedArr = {1, 2, 3, 4, 5};
+            // int n = reversedArr.length;
+            // int i = 0, j = n - 1;
+            // while (i < j) {
+            //     int temp = reversedArr[i];
+            //     reversedArr[i] = reversedArr[j];
+            //     reversedArr[j] = temp;
+            //     i++;
+            //     j--;
+            // }
+            // for (int element : reversedArr) {
+            //     System.out.print(element + " ");
+            // }
 
 
+            //Merge Two Sorted Arrays
 
+    //         int[] arr1 = {2, 6, 9};
+    //         int [] arr2 = {1,3, 4, 5, 7, 8};
 
+    //         int [] arr3 = new int [arr1.length + arr2.length];
 
+    //        for(int ele:arr3) System.out.print(ele + " ");
+    //        System.out.println();
+    //        merge (arr1, arr2, arr3);
+    //           for(int ele:arr3) System.out.print(ele + " ");
+    //           System.out.println();
+    // }
+    // public static void merge(int[] arr1, int[] arr2, int[] arr3) {
+    //      int i=0,j=0,k=0;
+    //      while(i<arr1.length && j<arr2.length) {
+    //         if(arr1[i]<arr2[j]) 
+    //             arr3[k++] = arr1[i++];
+    //           else arr3[k++] = arr2[j++];
+    //      }
 
+    //      if(i==arr1.length) {
+    //         while(j<arr2.length) {
+    //             arr3[k++] = arr2[j++];
+    //         }
+    //      }
+    //      else {
+    //         while(i<arr1.length) {
+    //             arr3[k++] = arr1[i++];
+    //         }
+    //     }
 
+        // other way
 
+            int[] arr1 = {2, 6, 9};
+            int [] arr2 = {1,3, 4, 5, 7, 8};
 
+            int [] arr3 = new int [arr1.length + arr2.length];
 
-        }
+           for(int ele:arr3) System.out.print(ele + " ");
+           System.out.println();
+           merge (arr1, arr2, arr3);
+              for(int ele:arr3) System.out.print(ele + " ");
+              System.out.println();
     }
+    public static void merge(int[] arr1, int[] arr2, int[] arr3) {
+         int i=arr1.length-1,j=arr2.length-1,k=arr3.length-1;
+         while(i>=0 && j>=0) {
+            if(arr1[i]>arr2[j]) 
+                arr3[k--] = arr1[i--];
+              else arr3[k--] = arr2[j--];
+         }
+
+         if(i==-1) {
+            while(j>=0) {
+                arr3[k--] = arr2[j--];
+            }
+         }
+         else {
+            while(i>=-1) {
+                arr3[k--] = arr1[i--];
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
