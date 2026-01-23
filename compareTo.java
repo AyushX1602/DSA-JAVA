75a4018b89cf0254cd3642445108591213be9b4e
AyushX1602 <ayushpathak16022005@gmail.com>
2026-01-23 23:21:23 +0530

    Add substring sum calculation to compareTo class

--- compareTo.java
+++ compareTo.java
@@ -17,5 +17,16 @@ public class compareTo {
         String a = "apple";   
         String b = "banana";
         System.out.println(compareStrings(b,a)); // Output will be negative since "apple" < "banana"
+    
+        String s = "88778";
+        long sum = 0; // Use long to avoid overflow for large numbers
+
+        for(int i = 0; i < s.length(); i++) {
+            for(int j = i; j < s.length(); j++) {
+                String sub = s.substring(i, j + 1); // Get substring
+                sum += Integer.parseInt(sub);       // Convert and add to sum
+            }
+        }
+        System.out.println("Sum of all substrings: " + sum);
     }
 }
